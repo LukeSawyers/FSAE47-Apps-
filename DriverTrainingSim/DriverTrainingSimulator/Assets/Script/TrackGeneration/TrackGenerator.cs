ab72d57a3fad93d688360723e3027e0061119997
luke sawyers <luke sawyers>
2017-03-06 21:47:28 +1300

    Added the ability to create a mesh from given xy coordinates and a trackwidth, discovered unity does not allow external resources to be loaded using Resources.Load, these assets are just included in the build, looking at using System.IO methods

--- DriverTrainingSim/DriverTrainingSimulator/Assets/Script/TrackGeneration/TrackGenerator.cs
+++ DriverTrainingSim/DriverTrainingSimulator/Assets/Script/TrackGeneration/TrackGenerator.cs
@@ -6,14 +6,22 @@ using System;
 
 public class TrackGenerator : MonoBehaviour {
 
+    // exposed game object
     public GameObject GeneratedTrack;
 
+    public Material TrackMaterial;
+
+    // file stuff
     private string Filename = "";
     private string RootDir = @"C:\Users\luke_\Desktop\FSAE Apps\DriverTrainingSim\DriverTrainingSimulator\Assets\Resources\";
     private float trackwidth = 2.5f;
 
+    // mesh stuff
     private GameObject[] TrackNodes;
-    
+    private Vector3[] Verticies;
+    private int[] Triangles;
+    private Vector3[] Normals;
+
     private void OnGUI()
     {
         Filename = GUI.TextArea(new Rect(10, 10, 100, 20), Filename);
@@ -24,7 +32,9 @@ public class TrackGenerator : MonoBehaviour {
 
         if (GUI.Button(new Rect(10, 50, 100, 20), "Load"))
         {
-            ReadFile(Filename);
+            string[] stringCoordinates = ReadFile(Filename);
+            StringCoordinatesToGameObjects(stringCoordinates);
+            CreateMesh();
         }
     }
     
@@ -33,15 +43,18 @@ public class TrackGenerator : MonoBehaviour {
     /// </summary>
     /// <param name="fileName">Path of the file in the resources folder to read</param>
     /// <returns>Success</returns>
-    private bool ReadFile(string fileName)
+    private string[] ReadFile(string fileName)
     {
+        // define return variable
+        string[] coordinates = new string[1] { "Error Getting Input" };
+
         // Handle any problems that might arise when reading the text
         try
         {
             string line;
 
             StreamReader theReader = new StreamReader(RootDir+fileName, Encoding.Default);
-
+            
             using (theReader)
             {
                 // While there's lines left in the text file, do this:
@@ -51,12 +64,8 @@ public class TrackGenerator : MonoBehaviour {
 
                     if (line != null)
                     {
-                        string[] coordinates = line.Split(';');
-                        if (coordinates.Length > 0)
-                        {
-                            LayTrack(coordinates);
-                        }
-                        else
+                        coordinates = line.Split(';');
+                        if (coordinates.Length < 1)
                         {
                             Debug.Log("No Entries! :/");
                         }
@@ -66,7 +75,7 @@ public class TrackGenerator : MonoBehaviour {
                 while (line != null);
 
                 theReader.Close();
-                return true;
+                
             }
         }
         // If anything broke in the try block, we throw an exception with information
@@ -74,15 +83,133 @@ public class TrackGenerator : MonoBehaviour {
         catch (Exception e)
         {
             Debug.Log(e.Message);
-            return false;
+        }
+        return coordinates;
+    }
+
+    /// <summary>
+    /// Converts a string array of coordinates to empty game objects and instansiates them
+    /// </summary>
+    /// <param name="StringCoordinates"></param>
+    private void StringCoordinatesToGameObjects(string[] StringCoordinates)
+    {
+        // initialise storage array
+        TrackNodes = new GameObject[StringCoordinates.Length];
+
+        // clear the previous track
+        for (int i = 0; i < GeneratedTrack.transform.childCount; i++)
+        {
+            Destroy(GeneratedTrack.transform.GetChild(i).gameObject);
+        }
+
+        // add objects
+        for (int i = 0; i < StringCoordinates.Length; i++)
+        {
+            // place the plane
+            string[] node = StringCoordinates[i].Split(',');
+            TrackNodes[i] = new GameObject();
+            TrackNodes[i].transform.parent = GeneratedTrack.transform;
+            TrackNodes[i].name = "Node";
+
+            try { TrackNodes[i].transform.position = new Vector3(float.Parse(node[0]), 0, float.Parse(node[1])); }
+            catch { }
+
+
+            // if is the first, 
+            if (i == 0)
+            {
+                TrackNodes[i].transform.localScale = new Vector3(0.1f * trackwidth, 1f, 0.1f);
+                continue;
+            }
+
+            // orient away from previous           
+            TrackNodes[i].transform.rotation = Quaternion.LookRotation(TrackNodes[i].transform.position - TrackNodes[i - 1].transform.position);
+
+            // if not last, slerp previous node towards current node
+            if (i != StringCoordinates.GetLength(0) - 1)
+            {
+                TrackNodes[i - 1].transform.rotation = Quaternion.Slerp(TrackNodes[i].transform.rotation, TrackNodes[i - 1].transform.rotation, 0.5f);
+            }
         }
     }
 
     /// <summary>
-    /// Lays a track from a specified array of coordinates for the central path through that track
+    /// Creates a mesh from the transforms of TrackNodes
+    /// </summary>
+    private void CreateMesh()
+    {
+        // Verticies
+        // Verticies are half the track width either side of each game object
+        Verticies = new Vector3[TrackNodes.Length * 2];
+        for(int i = 0; i < TrackNodes.Length; i++)
+        {
+            // left vertex
+            Verticies[i * 2] = TrackNodes[i].transform.position - TrackNodes[i].transform.right * trackwidth / 2f;
+
+            // right vertex
+            Verticies[(i * 2) + 1] = TrackNodes[i].transform.position + TrackNodes[i].transform.right * trackwidth / 2f;
+        }
+
+        // Triangles
+        Triangles = new int[(TrackNodes.Length - 1) * 6];
+        for (int i = 0; i < (TrackNodes.Length - 1); i++)
+        {
+            // bottom left triangle
+            Triangles[(i * 6) + 0] = (i * 2) + 0;
+            Triangles[(i * 6) + 1] = (i * 2) + 2;
+            Triangles[(i * 6) + 2] = (i * 2) + 1;
+
+            // top right triangle
+            Triangles[(i * 6) + 3] = (i * 2) + 2;
+            Triangles[(i * 6) + 4] = (i * 2) + 3;
+            Triangles[(i * 6) + 5] = (i * 2) + 1;
+
+        }
+
+        // Normals
+        Normals = new Vector3[Verticies.Length];
+        for(int i = 0; i < Normals.Length; i++)
+        {
+            Normals[i] = -Vector3.up;
+        }
+
+        // UVs? Nah 
+
+        // create mesh
+        var mf =  GeneratedTrack.GetComponent<MeshFilter>();
+        if(mf == null)
+        {
+            mf = GeneratedTrack.AddComponent<MeshFilter>();
+        }
+        
+        
+        Mesh m = new Mesh();
+        m.vertices = Verticies;
+        m.triangles = Triangles;
+        m.normals = Normals;
+        mf.mesh = m;
+        
+
+        var mr = GeneratedTrack.GetComponent<MeshRenderer>();
+        if(mr == null)
+        {
+            mr = GeneratedTrack.AddComponent<MeshRenderer>();
+        }
+        mr.material = TrackMaterial;
+
+        var mc = GeneratedTrack.GetComponent<MeshCollider>();
+        if (mc == null)
+        {
+            mc = GeneratedTrack.AddComponent<MeshCollider>();
+        }
+        mc.sharedMesh = m;
+    }
+
+    /// <summary>
+    /// Lays a track from a specified array of coordinates for the central path through that track using planes
     /// </summary>
     /// <param name="coordinates"> Array of coordinates to use</param>
-    private void LayTrack(string[] coordinates)
+    private void LayPlaneTrack(string[] coordinates)
     {
         // initialise storage array
         TrackNodes = new GameObject[coordinates.Length];
@@ -98,7 +225,7 @@ public class TrackGenerator : MonoBehaviour {
         {
             // place the plane
             string[] node = coordinates[i].Split(',');
-            TrackNodes[i] = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            TrackNodes[i] = GameObject.CreatePrimitive(PrimitiveType.Plane);
             TrackNodes[i].transform.parent = GeneratedTrack.transform;
             TrackNodes[i].name = "Node";
             TrackNodes[i].transform.position = new Vector3(float.Parse(node[0]), 0, float.Parse(node[1]));
@@ -119,13 +246,6 @@ public class TrackGenerator : MonoBehaviour {
             {
                 TrackNodes[i-1].transform.rotation = Quaternion.Slerp(TrackNodes[i].transform.rotation, TrackNodes[i-1].transform.rotation, 0.5f);
             }
-        }
-
-        // configure quads
-        foreach(GameObject o in TrackNodes)
-        {
-            Mesh m = new Mesh();
-
         }
     }
 }
