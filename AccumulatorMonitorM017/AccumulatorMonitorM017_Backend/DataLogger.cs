03f03d608aa59a3f39d6d20344b4a5a0eaec23e0
Luke Sawyers <luke_sawyers@hotmail.co.nz>
2017-06-17 22:10:22 +1200

    Initial implementation of the datalogger class, untested

--- AccumulatorMonitorM017/AccumulatorMonitorM017_Backend/DataLogger.cs
+++ AccumulatorMonitorM017/AccumulatorMonitorM017_Backend/DataLogger.cs
@@ -11,19 +11,80 @@ namespace AccumulatorMonitorM017.Backend
     /// </summary>
     class DataLogger
     {
+
+        string Dir;
+        string Path;
+        string Logfile;
+        string fullpath { get { return Dir + Path + Logfile; } }
+
         public DataLogger()
         {
-
+            Initialise("\\Log Files");
         }
 
-        public DataLogger(string fileLocation)
+        public DataLogger(string fileDirectory)
         {
-
+            Initialise(fileDirectory);
         }
 
+        private void Initialise(string fileDirectory)
+        {
+            Dir = AppDomain.CurrentDomain.BaseDirectory;
+            Path = fileDirectory;
+            Logfile = "\\LogFile" + DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt") + ".csv";
+            Logfile.Replace(':', '-');
+
+            // add the header
+            string[] s = {
+                "Accumulator Monitor Log File" + DateTime.Now.ToString(@"MM\/dd\/yyyy h\:mm tt"),
+                "",
+                "",
+                "",
+            };
+
+            for(int i = 0; i < 288; i++)
+            {
+                // every even cell
+                if ((i % 2) == 0) // i = 0, 2, 4 etc
+                {
+                    // Unit labels
+                    s[3] += "V";
+
+                    // Cell Number
+                    s[2] += "Cell " + (i % 24) + 1;
+                    
+                    // segment label header
+                    if ((i % 48) == 0) // i = 0, 48, 96 etc
+                    {
+                        s[1] += "Segment " + ((i / 48) + 1);
+                    }
+                }
+                // every odd cell
+                else
+                {
+                    // Unit labels
+                    s[3] += "T";
+                }
+                
+                // append commas 
+                s[1] += ",";
+                s[2] += ",";
+                s[3] += ",";
+
+            }
+
+            System.IO.File.AppendAllLines(fullpath, s);
+        }
+
+        /// <summary>
+        /// Logs a new dataframe to the file
+        /// </summary>
+        /// <param name="f"></param>
         public void Log(DataFrame f)
         {
-
+            string[] s = { "" };
+            int startInd = f.segmentID * 24;
+            //System.IO.File.AppendAllLines
         }
 
         public void Log(AccumulatorInterface acc)
